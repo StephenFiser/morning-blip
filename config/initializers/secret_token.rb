504b6364f1cffa90ac873863a65d395c41207004
Stephen Fiser <stephen.fiser@gmail.com>
2013-11-19 17:31:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MorningBlip::Application.config.secret_key_base = '0e7b8e22d7d2fb40b3e39115e44e45bc854cee10eb43ae0bed4fa870d4ad417a5defbefe16eb220d347970034af152f06d3cbcd2e75f309d716909cb1e91a208'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MorningBlip::Application.config.session_store :cookie_store, key: '_morning-blip_session'
